63bbd5512bfbdcb9fcd049317a2cd89ab61cb299
Ashwin Kannan <ashwin.kannan@berkeley.edu>
2019-08-23 03:01:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,3 +6,27 @@ def map_to_negativize(source_array)
   }
   return result
 end
+
+def map_to_no_change(source_array)
+  result = []
+  source_array.length.times { |i|
+    result[i] = source_array[i]
+  }
+  return result
+end
+
+def map_to_double(source_array)
+  result = []
+  source_array.length.times { |i|
+    result[i] = source_array[i] * 2
+  }
+  return result
+end
+
+def map_to_square(source_array)
+  result = []
+  source_array.length.times { |i|
+    result[i] = source_array[i] ** 2
+  }
+  return result
+end
